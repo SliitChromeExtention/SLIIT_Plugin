163b35c458f6e3c05bd3366c9557cba758e39620
therealtharindu <therealtharindu@gmail.com>
2021-04-06 12:12:25 +0530

    changes in content js for capturing data

--- content.js
+++ content.js
@@ -8,14 +8,28 @@ function onclick(){
   //console.log(document.hasFocus())
   // console.log('Active Element: ' +x);
   if(x == 'TEXTAREA'&& document.hasFocus()){
-    console.log('TEXTAREA active');
-    console.log(document.hasFocus())
+    // console.log('TEXTAREA active');
+    // console.log(document.hasFocus())
 
     document.addEventListener('keyup',onKeySpacePress);
     function onKeySpacePress(e){
       if(e.keyCode == 32){
         document.querySelectorAll('textarea').forEach((input) => {
-          console.log(input.value);
+          //console.log(input.value)
+          let wordlist = input.value
+          
+         
+          for(let i=0; i< wordlist.length; i++){
+            let words = wordlist.split(/(\s+)/).filter( e => e.trim().length > 0);
+            console.log(words[i])
+            
+          }
+
+
+        //  let wordlist = input.split(/(\s+)/).filter( e => e.trim().length > 0);
+        //  console.log(wordlist);
+          
+
         });
         
       }
