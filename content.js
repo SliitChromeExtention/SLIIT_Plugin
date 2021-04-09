6b75a95a12b3b11ff927b5e7119d2f7c82aca261
therealtharindu <therealtharindu@gmail.com>
2021-04-09 14:45:21 +0530

    change color fixed

--- content.js
+++ content.js
@@ -59,41 +59,7 @@ function onclick(){
         
        }
         
-        // document.querySelectorAll('mydiv').forEach((input) => {
-        //   //console.log(input.value)
-        //   let wordlist = input.value
-        //   //console.log('word list :'+wordlist)
-        //   let words = wordlist.split(/(\s+)/).filter( e => e.trim().length > 0)
-        //   //console.log(words)
-        //   for(let i=0; i<words.length; i++){
-        //     fetch('http://127.0.0.1:5000/api/spellchecking',{
-        //       method:'POST',
-        //       headers:{
-        //         'Content-Type':'application/json'
-        //       },
-        //       body: JSON.stringify({
-        //         "word": words[i].toString()
-        //       })
-        //     }).then(res => {
-        //       return res.json()
-        //     })
-        //     .then(data => {
-        //       if(data == false){
-        //         console.log('this word is incorrect :'+words[i])
-        //         //console.log(document.querySelector('textarea').style)
-        //         // document.querySelector('textarea').style.textDecoration = "line-through"
-        //         var txtArea = document.querySelector('div');
-        //         txtArea.value = txtArea.value.replace(words[i], words[i].fontcolor('red'));
-               
-        //       }
-        //     })
-        //     .catch(error => console.log('ERROR!!'))
-            
-            
-  
-        //   }
-          
-        // })
+
         
       }
       
